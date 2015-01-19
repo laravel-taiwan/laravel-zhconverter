b038a8d622aadb17da1ce8423304c7f9bb1212ff
Seta <seta@deansoft.com.tw>
2015-01-19 11:17:48 +0800

    v0.0.1

---
+++ src/Seta0909/LaravelZhconverter/Facades/LaravelZhconverter.php
@@ -0,0 +1,15 @@
+<?php namespace Seta0909\LaravelZhconverter\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class LaravelZhconverter extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'ZhConvert'; }
+
+}
---
+++ src/Seta0909/LaravelZhconverter/LaravelZhconverter.php
@@ -0,0 +1,47 @@
+<?php
+    namespace Seta0909\LaravelZhconverter;
+
+    /**
+     * Created by PhpStorm.
+     * User: Seta
+     * Date: 2015/1/19
+     * Time: 上午 09:45
+     */
+
+    class LaravelZhconverter
+    {
+        private static $instance;
+        protected static $cnCode = [];
+        protected static $twCode = [];
+
+        private static function getInstance()
+        {
+            if (!isset(self::$instance)) {
+                require('ZhConversion.php');
+                $class          = __CLASS__;
+                self::$instance = new $class();
+                self::$cnCode   = $zh2Hant;
+                self::$twCode   = $zh2Hans;
+            }
+        }
+
+        public static function test()
+        {
+            self::getInstance();
+            echo self::translate('測試中', 'CN');
+        }
+
+        public static function translate($words, $type)
+        {
+            $translated = '';
+            for ($i = 0; $i < mb_strlen($words); $i++) {
+                $word = mb_substr($words, $i, 1, "utf-8");
+                if ($type == 'CN') {
+                    $translated .= (isset(self::$twCode[$word])) ? self::$twCode[$word] : $word;
+                } else {
+                    $translated .= (isset(self::$cnCode[$word])) ? self::$cnCode[$word] : $word;
+                }
+            }
+            return $translated;
+        }
+    }
